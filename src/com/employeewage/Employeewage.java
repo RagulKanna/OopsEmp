a7c50f499f959d789d410980d04bd4d55791bb95
RagulKanna <ragulragi619@gmail.com>
2021-09-02 14:31:07 +0530

    Added:Calculate parttime employee's wage

--- src/com/employeewage/Employeewage.java
+++ src/com/employeewage/Employeewage.java
@@ -3,8 +3,11 @@ package com.employeewage;
 class Employee
 {
 
-    public static int WAGE_PER_HOUR = 20,FULL_DAY_HOUR = 8;
+    int checkfullhours = (int) (Math.floor(Math.random() * 8 % 9) + 1);
+    int checkparthours = (int) (Math.floor(Math.random() * 4 % 5) + 1);
+    public static int WAGE_PER_HOUR = 20,FULL_DAY_HOUR = 8,PART_TIME_HOUR=4;
     public static int check = (int) (Math.random() * 2);
+
     public void employeePresentorAbesnt()
     {
 
@@ -17,16 +20,15 @@ class Employee
 
     }
 
-    public void dailyemployeewage()
+    public void dailyemployeewage(int checkhours,int hour)
     {
         System.out.println("\n\ncheck the employee daily wage:");
 
-        int fullpresent = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        int fullpresent = WAGE_PER_HOUR * hour;
         System.out.println("\n  if employee worker is present for full day, the salary is " + fullpresent+"...");
 
         if (check == 1)
         {
-            int checkhours = (int) (Math.floor(Math.random() * 8 % 9) + 1);
             int hoursbased = checkhours * WAGE_PER_HOUR;
             System.out.println("\n  employee's salary based on hours, employee present for " + checkhours + " hours so his salary is " + hoursbased+"...");
         }
@@ -37,13 +39,39 @@ class Employee
     }
 }
 
+class Fulltimeemployee extends Employee
+{
+
+    public void employee()
+    {
+        System.out.println("\n\n   For Fulltime Employee");
+        this.employeePresentorAbesnt();
+        this.dailyemployeewage(checkfullhours,FULL_DAY_HOUR);
+    }
+
+}
+
+class Parttimeemployee extends Employee
+{
+
+    public void employee()
+    {
+        System.out.println("\n\n   For Parttime Employee");
+        this.employeePresentorAbesnt();
+        this.dailyemployeewage(checkparthours,PART_TIME_HOUR);
+    }
+
+}
+
+
 public class Employeewage
 {
         public static void main (String[] args)
         {
-            Employee emp = new Employee();
-            emp.employeePresentorAbesnt();
-            emp.dailyemployeewage();
+            Fulltimeemployee fe =new Fulltimeemployee();
+            fe.employee();
+            Parttimeemployee pe=new Parttimeemployee();
+            pe.employee();
         }
 }
 
