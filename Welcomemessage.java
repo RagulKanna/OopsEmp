3b81e21be77b6442d8902e2f565dfa60b0790939
RagulKanna <ragulragi619@gmail.com>
2021-09-02 13:36:13 +0530

    Added:Welcome Message

---
+++ Welcomemessage.java
@@ -0,0 +1,5 @@
+public class Welcomemessage {
+    public static void main(String[] args) {
+        System.out.println("\n\n     Welcome to Employee Wage Computation Program with OOPS Concept");
+    }
+}
